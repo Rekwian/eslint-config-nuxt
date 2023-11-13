e5e90fc1e4ba37d5fcf8110512b1129e3ba3f6fb
Jimmy <j.bailleux@proprietes-privees.fr>
2023-11-13 08:24:26 +0100

    firt commit

---
+++ index.js
@@ -0,0 +1,79 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue-pug/vue3-recommended',
+  ],
+  ignorePatterns: [
+    '**/node_modules/',
+    '**/vendor/',
+    '**/vendors/',
+    'out/',
+    'dist/',
+    'build/',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      files: ['**/*.js', '**/*.ts', '**/*.mjs', '**/*.mts', '**/*.cjs', '**/*.cts', '**/*.vue']
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    'no-unused-vars': ['error', { argsIgnorePattern: '_' }],
+    'object-curly-newline': 'off',
+    'sort-keys': ['warn', 'asc'],
+    'no-param-reassign': 0,
+    'no-undef': 0,
+    'vue/block-lang': ['error', {
+      script: { lang: ['js'] },
+      style: {
+        lang: ['sass', 'scss', 'css'],
+      },
+      template: {
+        lang: 'pug',
+      },
+    }],
+    'vue/block-order': ['error', {
+      order: ['template', 'script', 'style'],
+    }],
+    'vue/component-name-in-template-casing': ['error', 'kebab-case', { registeredComponentsOnly: false }],
+    'vue/html-quotes': 0,
+    'vue/max-attributes-per-line': 0,
+    'vue/multi-word-component-names': 0,
+  },
+  settings: {
+    'import/resolver': {
+      'eslint-import-resolver-custom-alias': {
+        alias: {
+          '@': './',
+          assets: './assets',
+          store: './store',
+          '~': './',
+        },
+        extensions: ['.js', '.ts', '.mjs', '.mts', 'cjs', '.cts', '.vue'],
+      },
+    },
+  },
+};
